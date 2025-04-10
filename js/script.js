01f75736c93a2510ceb09c19ee3208a0a4a22554
EDYCINDY <redg16253@gmail.com>
2025-04-10 11:16:07 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
         const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx8Nlq71lciAyzO8JhekkJuB9BxMV0v4vgRMC5nQ3Z5exX_YpeVSrGEIdOfqK0ePxkp/exec";
         // !!! GANTI USERNAME & REPO_NAME untuk preview URL !!!
         const githubUsername = "edycindy";
-        const githubRepoName = "Shortener";
+        const githubRepoName = "Bucin";
 
         function generateRandomString(length = 6) {
             const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
